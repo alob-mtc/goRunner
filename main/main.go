5e37285bcf90107355c6a1fd83b6462b5db84d29
Akinlua Bolamigbe Jomiloju <bolamigbeakinlua@gmail.com>
2020-06-21 19:42:32 +0100

    fix a bug with completeMain chan

--- main/main.go
+++ main/main.go
@@ -12,17 +12,17 @@ import (
 )
 
 // timeout is the number of second the program has to finish.
-const timeout = 2 * time.Second
+const timeout = 3 * time.Second
 
 // main is the entry point for the program.
 func main() {
 	log.Println("Starting work.")
 
-	// Create a new timer value for this run.
-	r := runner.New(timeout)
+	// Create a new timer value for this run and the number of worker.
+	r := runner.New(timeout, 3)
 
 	// Add the tasks to be run.
-	r.Add(createTask(), createTask(), createTask())
+	r.Add(createTask("A"), createTask("B"), createTask("C"), createTask("D"), createTask("E"), createTask("F"))
 
 	// Run the tasks and handle the result.
 	if err := r.Start(); err != nil {
@@ -41,11 +41,11 @@ func main() {
 
 // createTask returns an example task that sleeps for the specified
 // number of seconds based on the id.
-func createTask() func(int) {
+func createTask(name string) func(int) {
 	return func(id int) {
 		duration := 1
-		log.Printf("Processor - Task #%d.\n", id)
+		log.Printf("Processor - Task #%s....worker #%d\n", name, id)
 		time.Sleep(time.Duration(time.Duration(duration)) * time.Second)
-		log.Printf("Processor - Task #%d., completed Time - #%d ", id, duration)
+		log.Printf("Processor - Task #%s., completed Time - #%d ", name, duration)
 	}
 }
